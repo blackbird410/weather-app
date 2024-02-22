91dfc92d971dd3b22f3417029e61d3567a8a4636
Neil T. Rigaud <ntsrigaud14@gmail.com>
2024-02-23 06:31:46 +0800

    Added page icon

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./style.css";
+import pageIcon from "./cloudy-outline.svg";
 import { fetchData, displayMain, displayWeather } from "./functions.js";
 
+const link = document.createElement("link");
+link.setAttribute("rel", "icon");
+link.setAttribute("type", "image/x-icon");
+link.setAttribute("href", pageIcon);
+document.head.appendChild(link);
 displayMain();
